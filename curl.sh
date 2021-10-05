0006ca14b8c23892c0111050ba632b10f89e4d41
nexomcoin <91990733+nexomcoin@users.noreply.github.com>
2021-10-06 04:47:08 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,4 +1,12 @@
-while true; do
-    sleep 5
-    curl -L https://nexom.onrender.com
-done
+#while true; do
+ #   sleep 5
+  #  curl -L https://nexom.onrender.com
+#done
+wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz > /dev/null
+mkdir OtohitsApp
+cd OtohitsApp
+echo "/login:5d58ca31-905f-4cae-bf82-c8e8eee94711" > otohits.ini
+echo "/autoupdate" >> otohits.ini
+tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
+
+./otohits-app
