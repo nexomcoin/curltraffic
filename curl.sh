1fce02e1e2c5c6a1248d9a91e3820777c841e6a9
nexomcoin <91990733+nexomcoin@users.noreply.github.com>
2021-10-06 03:51:18 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,6 +1,6 @@
-#xtxr=$(curl -v -H -O https://nexom.onrender.com)
+xtxr=$(curl -v -H -O https://nexom.onrender.com)
 #echo $abc
-watch -n 5 "curl -L https://google.com"
-#while sleep 3;
-#do "curl -L https://nexom.onrender.com"; done
+#watch -n 5 "curl -L https://google.com"
+while sleep 3;
+do $xtxr; done
 
